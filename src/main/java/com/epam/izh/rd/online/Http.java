3379d72c95786425605075f850e5c67e01871278
maxorax <orlov3020max@gmail.com>
2020-06-30 23:35:11 +0400

    Создаk ветку feature . Добавил зависимость com.fasterxml.jackson.core.jackson-databind - библиотека для маппинга JSON-файлов на объекты Доработал сущность Pokemon, для возможности маппинга JSON-документа. Имплементировал класс ObjectMapperFactory - он возвращает объект, с помощью которого мы будем совершать маппинг Имплементировал PokemonFethingService и PokemonFightingClubService. В методе main написал демо, которое демонстрирует простой бой между покемонами Pikachu и Slowpoke. *Имплементировал юнит-тесты PokemonFethingService и PokemonFightingClubService. Для заглушки сервиса Poke-api использовал Wiremock.

--- src/main/java/com/epam/izh/rd/online/Http.java
+++ src/main/java/com/epam/izh/rd/online/Http.java
@@ -1,7 +1,15 @@
 package com.epam.izh.rd.online;
 
-public class Http {
-    public static void main(String[] args) {
+import com.epam.izh.rd.online.entity.Pokemon;
+import com.epam.izh.rd.online.service.PokemonFetchingServices;
+import com.epam.izh.rd.online.service.PokemonFigthingClub;
 
+public class Http {
+
+    public static void main(String[] args) {
+        PokemonFetchingServices pokemonFetchingServices = new PokemonFetchingServices();
+        Pokemon pikachu = pokemonFetchingServices.fetchByName("pikachu");
+        Pokemon slowpoke = pokemonFetchingServices.fetchByName("slowpoke");
+        System.out.println(new PokemonFigthingClub().doBattle(slowpoke,pikachu).getPokemonName() + " win!");
     }
 }
